5ecd75844f357ccb740c4429b16e16e87b05f7e4
canikwe <c.anikwe@gmail.com>
2020-03-31 21:19:19 -0400

    add update option to create group container

--- src/containers/CreateGroupContainer.js
+++ src/containers/CreateGroupContainer.js
@@ -1,11 +1,16 @@
 import React from "react"
+import {useSelector, shallowEqual} from 'react-redux'
 import ActivityContainer from "./ActivityContainer"
 import GroupForm from './GroupForm'
 
 const CreateGroupContainer = () => {
+  const {group} = useSelector(state => ({
+    group: state.selectedGroup
+  }), shallowEqual)
+  
   return (
     <>
-      <h3 className='header'>New Pair</h3>
+      <h3 className='header'>{group.id ? 'Update' : 'New'} Pair</h3>
       <ActivityContainer />
       <GroupForm />
     </>
